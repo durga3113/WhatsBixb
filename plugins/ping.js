3efe685960d029086ab20fe747cd4ca75d14868b
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-12-07 23:32:08 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -4,7 +4,7 @@ you may not use this file except in compliance with the License.
 WhatsBixby - Ziyan
 */
 
-const { Bixby ,isPrivate} = require("../lib/");
+const { Bixby ,isPublic } = require("../lib/");
 
 Bixby(
   {
