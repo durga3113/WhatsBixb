70061192ed228515d4a01e15d1f4d2baaedf9ec1
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-12-07 11:40:43 +0530

    Fix🥱

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ Licensed under the MIT License;
 you may not use this file except in compliance with the License.
 WhatsBixby - Ziyan
 */
+
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 if (fs.existsSync("config.env"))
@@ -10,27 +11,27 @@ if (fs.existsSync("config.env"))
 
 const toBool = (x) => x == "true";
 
-DATABASE_URL = process.env.DATABASE_URL || "./database.db";
+DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
 let HANDLER = "false";
 module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID: process.env.SESSION_ID ||false,
+  SESSION_ID:process.env.SESSION_ID || "",
   LANG: process.env.LANG || "EN",
-  HANDLERS:
-    process.env.HANDLER === "false" || process.env.HANDLER === "null"
-      ? "^"
-      : "^",
+  HANDLERS: process.env.PREFIX || '[.]',
+  BOT_INFO : "",
   RMBG_KEY: process.env.RMBG_KEY || false,
   BRANCH: "master",
   PACKNAME: process.env.PACKNAME || "WhatsBixby",
-  WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
+  WELCOME_MSG:
+    process.env.WELCOME_MSG ||
+    "Hi @user Welcome to @gname",
   GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
-  AUTHOR: process.env.AUTHOR || "DX-MODS",
+  AUTHOR: process.env.AUTHOR || "Ziyan",
   DATABASE_URL: DATABASE_URL,
   DATABASE:
-    DATABASE_URL === "./database.db"
+    DATABASE_URL === "./lib/database.db"
       ? new Sequelize({
           dialect: "sqlite",
           storage: DATABASE_URL,
@@ -46,11 +47,10 @@ module.exports = {
           },
           logging: false,
         }),
-  SUDO: process.env.SUDO || " ",
+  SUDO: process.env.SUDO || "919446072492",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
-  OWNER_NAME: process.env.OWNER_NAME || "Ziyan",
-  BOT_NAME: process.env.BOT_NAME || "WhatsBixby",
-  CAPTION: process.env.CAPTION || "Made By WhatsBixby",
+  OWNER_NAME: process.env.OWNER_NAME || "ᴛᴏxɪᴄʟᴇᴏ-ᴍᴅ",
+  BOT_NAME: process.env.BOT_NAME || "whatsbixby",
   WORK_TYPE: process.env.WORK_TYPE || "public",
 };
