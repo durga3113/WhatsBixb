465eb7fba2a8abb6db65277220423cbd7831fee6
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-12-07 11:53:15 +0530

    Fix✅

---
+++ plugins/ping.js
@@ -0,0 +1,19 @@
+const { Bixby ,isPrivate} = require("../lib/");
+
+
+Bixby(
+  {
+    pattern: "ping",
+    fromMe: isPrivate,
+    desc: "To check ping",
+    type: "user",
+  },
+  async (message, match) => {
+    const start = new Date().getTime();
+    await message.sendMessage(" *Testing Speed!* ");
+    const end = new Date().getTime();
+    return await message.sendMessage(
+      " *Response in !* " + (end - start) + " ms"
+    );
+  }
+);
