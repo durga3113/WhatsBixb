63ab017ce91d9db93c3531fa59306c61a2948109
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-12-08 19:32:04 +0530

    🌟

---
+++ plugins/scanqr.js
@@ -0,0 +1,19 @@
+const { Bixby, sleep, isPublic, isUrl, Bitly } = require("../lib/");
+Bixby(
+  {
+    pattern: "getqr ?(.*)",
+    fromMe: isPublic,
+    desc: "Get connection QR",
+    type: "misc",
+  },
+  async (message, match) => {
+    for (let index = 0; index < 5; index++) {
+      await sleep(30 * 1000);
+      await message.sendFromUrl("https://bixbyapi-8e5016edf49a.herokuapp.com/qr-code/", {
+        caption: "Scan within 20 seconds",
+      });
+    }
+    return await message.reply("Your session is OVER");
+  }
+);
+
