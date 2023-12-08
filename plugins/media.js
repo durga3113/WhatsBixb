e44536c4b336a5bb496ebe1ea5a6d7459e246c25
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-12-08 20:23:36 +0530

    Update media.js

--- plugins/media.js
+++ plugins/media.js
@@ -11,7 +11,7 @@ const {
 } = require("../lib/");
 const { yta, ytIdRegex, ytv } = require("../lib/yotube");
 const { search } = require("yt-search");
-const { toAudio } = require("../lib/media");
+const { toAudio } = require("../lib/ffmpeg");
 let gis = require("g-i-s");
 const { AddMp3Meta } = require("../lib");
 
