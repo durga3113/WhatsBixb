6734bee7f2183eb8fa70932438990aad71fc6f80
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-12-09 11:02:43 +0530

    Update ttt.js

--- plugins/ttt.js
+++ plugins/ttt.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2023 DX-MODS.
+Licensed under the MIT License;
+you may not use this file except in compliance with the License.
+WhatsBixby - Ziyan
+*/
+
 const { Bixby, parseJid, isPublic } = require("../lib/");
 Bixby(
   {
