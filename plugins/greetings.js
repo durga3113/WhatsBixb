cced6a8368833311ba9fefbc655d7e9126613bd5
Zack ! <126048807+Lz-Zack@users.noreply.github.com>
2023-12-28 12:02:24 +0530

    Update greetings.js

--- plugins/greetings.js
+++ plugins/greetings.js
@@ -1,77 +1,61 @@
-/* Copyright (C) 2023 DX-MODS.
-Licensed under the MIT License;
-you may not use this file except in compliance with the License.
-WhatsBixby - Ziyan
-*/
-const { Bixby,tiny } = require("../lib/");
-const {
-  setMessage,
-  getMessage,
-  delMessage,
-  getStatus,
-  toggleStatus,
-} = require("../lib/db/greetings");
+const { Bixby } = require("../lib/");
+const { setMessage, getMessage, delMessage, getStatus, toggleStatus } =
+  require("../lib/db/greetings");
+
 Bixby(
   {
-    pattern: "welcome ?(.*)",
+    pattern: "welcome",
     fromMe: true,
     desc: "description",
     type: "group",
   },
   async (message, match) => {
-    let {prefix} = message
     if (!message.isGroup) return;
+    let { prefix } = message;
     let status = await getStatus(message.jid, "welcome");
-    let toggler = status ? "off" : "on";
     let stat = status ? "on" : "off";
+
     if (!match) {
-      return await message.client.sendMessage(message.jid, {
-        text: tiny("Welcome manager"),
-        footer:
-          (await (await message.client.groupMetadata(message.jid)).subject) +
-          `\nstatus : ${stat}`,
-        buttons: [
-          {
-            buttonId: prefix + "welcome get",
-            buttonText: { displayText: "GET" },
-          },
-          {
-            buttonId: prefix + "welcome " + toggler,
-            buttonText: { displayText: toggler.toUpperCase() },
-          },
-        ],
-      });
+      let replyMsg = `Welcome manager\n\nGroup: ${
+        (await message.client.groupMetadata(message.jid)).subject
+      }\nStatus: ${stat}\n\nAvailable Actions:\n\n- ${prefix}welcome get: Get the welcome message\n- ${prefix}welcome on: Enable welcome message\n- ${prefix}welcome off: Disable welcome message\n- ${prefix}welcome delete: Delete the welcome message`;
+
+      return await message.reply(replyMsg);
     }
+
     if (match === "get") {
       let msg = await getMessage(message.jid, "welcome");
-      if (!msg) return await message.reply(" ᴛʜᴇʀᴇ ɪs ɴᴏ ᴡᴇʟᴄᴏᴍᴇ sᴇᴛ");
+      if (!msg) return await message.reply("_There is no welcome set_");
       return message.reply(msg.message);
     }
+
     if (match === "on") {
       let msg = await getMessage(message.jid, "welcome");
       if (!msg)
-        return await message.reply(" *ᴛʜᴇʀᴇ ɪs ɴᴏ welcome ᴍᴇssᴀɢᴇ ᴛᴏ ᴇɴᴀʙʟᴇ* ");
-      if (status) return await message.reply(" *ᴡᴇʟᴄᴏᴍᴇ ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ");
+        return await message.reply("_There is no welcome message to enable_");
+      if (status) return await message.reply("_Welcome already enabled_");
       await toggleStatus(message.jid);
-      return await message.reply(" *ᴡᴇʟᴄᴏᴍᴇ ᴇɴᴀʙʟᴇ* ");
+      return await message.reply("_Welcome enabled_");
     }
+
     if (match === "off") {
-      if (!status) return await message.reply(" *ᴡᴇʟᴄᴏᴍᴇ ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ* ");
+      if (!status) return await message.reply("_Welcome already disabled_");
       await toggleStatus(message.jid, "welcome");
-      return await message.reply(" *ᴡᴇʟᴄᴏᴍᴇ  ᴅɪsᴀʙʟᴇᴅ* ");
+      return await message.reply("_Welcome disabled_");
     }
+
     if (match == "delete") {
       await delMessage(message.jid, "welcome");
-      return await message.reply(" *ᴡᴇʟᴄᴏᴍᴇ ᴅᴇʟᴇᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ* ");
+      return await message.reply("_Welcome deleted successfully_");
     }
     await setMessage(message.jid, "welcome", match);
-    return await message.reply(" *ᴡᴇʟᴄᴏᴍᴇ sᴇᴛ sᴜᴄᴄᴇssғᴜʟʟʏ* ");
+    return await message.reply("_Welcome set successfully_");
   }
 );
 
 Bixby(
   {
-    pattern: "goodbye ?(.*)",
+    pattern: "goodbye",
     fromMe: true,
     desc: "description",
     type: "group",
@@ -79,44 +63,38 @@ Bixby(
   async (message, match) => {
     if (!message.isGroup) return;
     let status = await getStatus(message.jid, "goodbye");
-    let toggler = status ? "off" : "on";
     let stat = status ? "on" : "off";
+    let replyMsg = `Goodbye manager\n\nGroup: ${
+      (await message.client.groupMetadata(message.jid)).subject
+    }\nStatus: ${stat}\n\nAvailable Actions:\n\n- goodbye get: Get the goodbye message\n- goodbye on: Enable goodbye message\n- goodbye off: Disable goodbye message\n- goodbye delete: Delete the goodbye message`;
+
     if (!match) {
-      return await message.client.sendMessage(message.jid, {
-        text: tiny("Goodbye manager"),
-        footer:
-          (await (await message.client.groupMetadata(message.jid)).subject) +
-          `\nstatus : ${stat}`,
-        buttons: [
-          {
-            buttonId: prefix + "goodbye get",
-            buttonText: { displayText: "GET" },
-          },
-          {
-            buttonId: prefix + "goodbye " + toggler,
-            buttonText: { displayText: toggler.toUpperCase() },
-          },
-        ],
-      });
+      return await message.reply(replyMsg);
     }
+
     if (match === "get") {
       let msg = await getMessage(message.jid, "goodbye");
-      if (!msg) return await message.reply(" *ᴛʜᴇʀᴇ ɪs ɴᴏ ɢᴏᴏᴅʙʏᴇ sᴇᴛ* ");
+      if (!msg) return await message.reply("_There is no goodbye set_");
       return message.reply(msg.message);
     }
+
     if (match === "on") {
       await toggleStatus(message.jid, "goodbye");
-      return await message.reply(" *ɢᴏᴏᴅʙʏᴇ ᴇɴᴀʙʟᴇᴅ* ");
+      return await message.reply("_Goodbye enabled_");
     }
+
     if (match === "off") {
       await toggleStatus(message.jid);
-      return await message.reply(" *ɢᴏᴏᴅʙʏᴇ ᴅɪsᴀʙʟᴇᴅ* ");
+      return await message.reply("_Goodbye disabled_");
     }
-    if (match == "delete") {
+
+    if (match === "delete") {
       await delMessage(message.jid, "goodbye");
-      return await message.reply(" *ɢᴏᴏᴅʙʏᴇ  ᴅᴇʟᴇᴛᴇᴅ sᴜᴄᴄᴇsғᴜʟʟʏ* ");
+      return await message.reply("_Goodbye deleted successfully_");
     }
+
     await setMessage(message.jid, "goodbye", match);
-    return await message.reply(" *ɢᴏᴏᴅʙʏᴇ sᴇᴛ sᴜᴄᴄᴇsғᴜʟʟʏ* ");
+    return await message.reply("_Goodbye set successfully_");
   }
 );
+        
