61c8c6c40ac560d6e24a84084c307e245d0dbc50
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-29 01:54:09 +0530

    🌟

---
+++ plugins/encrypter-decrypter.js
@@ -0,0 +1,30 @@
+/* Copyright (C) 2024 DX-MODS.
+Licensed under the MIT License;
+you may not use this file except in compliance with the License.
+WhatsBixby - Ziyan
+*/
+
+const { Bixby, isPublic } = require("../lib/");
+const { eBinary, dBinary } = require("../lib/functions/binary");
+
+Bixby({
+          pattern: 'ebinary ?(.*)',
+          fromMe: isPublic,
+          desc: 'encrypt text to binary',
+          type: 'converter'}, 
+          async (m, match) => {
+match = match || m.reply_message.text
+if (!match) return await m.reply("Give me a text to encrypt")
+m.reply(await eBinary(match));
+});
+
+Bixby({
+          pattern: 'dbinary ?(.*)',
+          fromMe: isPublic,
+          desc: 'decrypt binary to text',
+          type: 'converter'}, 
+          async (m, match) => {
+match = match || m.reply_message.text
+if (!match) return await m.reply("Give me a binary code")
+m.reply(await dBinary(match));
+});
