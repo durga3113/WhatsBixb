0091b7f14ec5ef90acb88a76a0893e0d0d2358f4
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-02-11 15:02:11 +0530

    Fisked✅

--- plugins/tts.js
+++ plugins/tts.js
@@ -5,7 +5,7 @@ WhatsBixby - Ziyan
 */
 
 const { Bixby, isPublic } = require("../lib/");
-const { getAitts } = require("../lib/tts");
+const { getAitts, sendTts } = require("../lib/tts");
 
 Bixby(
         {
@@ -17,4 +17,15 @@ Bixby(
 },
 		async (message, match) => {			
         await getAitts(message, match);
+})
+
+Bixby(
+  {
+    pattern: "tts",
+    fromMe: isPublic,
+    desc: "text to speech",
+    type: "converter",
+  },  
+  async (message, match) => {			
+  await sendTts(message, match);
 })
