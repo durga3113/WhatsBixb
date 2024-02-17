680ea134a5eb2d1940e34bafea66f206ddc242de
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-02-17 12:06:22 +0530

    Fisked✅

--- plugins/misc.js
+++ plugins/misc.js
@@ -1,1 +1,24 @@
+/* Copyright (C) 2024 DX-MODS.
+Licensed under the MIT License;
+you may not use this file except in compliance with the License.
+WhatsBixby - Ziyan
+*/
 
+const { Bixby, sleep, isPublic } = require("../lib/");
+Bixby(
+  {
+    pattern: "getqr ?(.*)",
+    fromMe: isPublic,
+    desc: "Get connection QR",
+    type: "misc",
+  },
+  async (message, match) => {
+    for (let index = 0; index < 5; index++) {
+      await sleep(30 * 1000);
+      await message.sendFromUrl("${BASE_URL}qr-code/", {
+        caption: "Scan within 20 seconds",
+      });
+    }
+    return await message.reply("Your session is OVER");
+  }
+);
