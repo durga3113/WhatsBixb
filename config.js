256ba3edcc5287920c206ab01bbcd1af16cf0b19
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-02-20 13:59:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,10 +22,7 @@ module.exports = {
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
   SESSION_ID:process.env.SESSION_ID || "",
   LANGUAGE: process.env.LANGUAGE || "english",
-  PREFIX:
-    process.env.PREFIX === "false" || process.env.PREFIX === "null"
-      ? "^"
-      : "^[#]",
+  PREFIX: process.env.PREFIX || '[.]',
   BOT_INFO : "",
   RMBG_KEY: process.env.RMBG_KEY || false,
   IMGBB_KEY: process.env.IMGBB_KEY || "",
