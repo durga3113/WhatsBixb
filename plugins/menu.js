f677fb5fdd70026c0c030f40e4bbcd80750f449b
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-04-16 12:33:51 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,11 @@
+const { Bixby, isPublic } = require("../lib/");
+const { sendMenu } = require("../lib/functions");
+Bixby({
+ pattern: "menu",
+ fromMe: isPublic,
+ desc: "show your menu items",
+ type: "information",
+ },
+ async (message, match) => {			
+ return await sendMenu(message);
+})
