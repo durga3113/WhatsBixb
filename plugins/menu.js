126a506f42006c53c24c630a7b7891a2458f64d1
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-04-16 19:50:20 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,5 +1,12 @@
+/* Copyright (C) 2024 DX-MODS.
+Licensed under the MIT License;
+you may not use this file except in compliance with the License.
+WhatsBixby - Ziyan
+*/
+
 const { Bixby, isPublic } = require("../lib/");
 const { sendMenu } = require("../lib/functions");
+
 Bixby({
  pattern: "menu",
  fromMe: isPublic,
