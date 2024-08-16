d6544301496b8f4c8cdfb5a863578fbf9ad5bc3a
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-08-17 01:21:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const config = require("./config");
 const connect = require("./lib/connection");
 const { getandRequirePlugins } = require("./lib/db/plugins");
-const { GenSession } = require("./lib");
+const { GenSession } = require("./lib/functions");
 
 global.__basedir = __dirname;
 
